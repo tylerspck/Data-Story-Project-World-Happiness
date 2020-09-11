84e6b25292ba52de3b43a5f0f3e5bdf8776ceb56
tylerspck <60820827+tylerspck@users.noreply.github.com>
2020-09-10 20:21:37 -0400

    adding start of flask app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc, inspect
+from models import db
 
 from flask import Flask, jsonify
 from datetime import datetime as dt, datetime
@@ -12,7 +13,7 @@ from datetime import timedelta
 
 
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-    user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+    user=postgres, pw=postgres, url=POSTGRES_URL, db=happinessDB)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 # silence the deprecation warning
@@ -20,5 +21,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
+db.init_app(app)
 app = Flask(_name_)
