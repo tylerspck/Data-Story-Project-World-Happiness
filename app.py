3afc7d01cce2d74f8a602dc7d27c4657d0a9b2ed
tylerspck <60820827+tylerspck@users.noreply.github.com>
2020-09-12 18:25:44 -0400

    flask app update

--- app.py
+++ app.py
@@ -10,95 +10,283 @@ from sqlalchemy import create_engine, func, desc, inspect
 from flask import Flask, jsonify
 from datetime import datetime as dt, datetime
 from datetime import timedelta
-
-
-from sqlalchemy import create_engine
+import psycopg2
+import json
+from flask_cors import CORS, cross_origin
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/happinessDB')
+con = engine.connect()
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
 
-Base.classes.keys()
-
-happiness = Base.classes.happiness
-countrycodes = Base.classes.countrycodes
-wbcountries = Base.classes.wbcountries
-
-session = Session(engine)
 # Flask Setup
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def homepage():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
+        f"/api/v1.0/All<br/>"
         f"/api/v1.0/2015<br/>"
         f"/api/v1.0/2016<br/>"
         f"/api/v1.0/2017<br/>"
         f"/api/v1.0/2018<br/>"
         f"/api/v1.0/2019<br/>"
         f"/api/v1.0/2020<br/>"
-        f"/api/v1.0/All<br/>"
     )
 
+# @app.route("/api/v1.0/All")
+# def happinessAll():
+
+#      resultall = con.execute("""with result as (
+#         select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+#         select * from result""")
+
+#     results = []
+
+#     for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in resultall:
+#                 newjson = {}
+#                 newjson["country"] = country
+#                 newjson["region"] = region
+#                 newjson["country code"] = countrycode
+#                 newjson["capital city"] = capitalcity
+#                 newjson["country"] = country
+#                 newjson["latitude"] = capitallat
+#                 newjson["longitude"] = capitallong
+#                 newjson["happiness Rank"] = happiness_rank
+#                 newjson["Year"] = year
+#                 newjson["happiness_score"] = happiness_score
+#                 newjson["economy"] = economy
+#                 newjson["family"] = family
+#                 newjson["health"] = health
+#                 newjson["freedom"] = freedom
+#                 newjson["trust"] = trust
+#                 newjson["generosity"] = generosity
+#                 newjson["population"] = population
+#                 newjson["area"] = area
+#                 newjson["gdp"] = gdp
+#                 newjson["income level"] = incomelevel
+#                 results.append(newjson)
+        
+#     return jsonify(results)
 
 @app.route("/api/v1.0/2015")
 def happiness2015():
-    # Create our session (link) from Python to the DB
-    """Convert the query results to a dictionary using date as the key and prcp as the value."""
-    # Query all passengers
-    results2015 = session.query(happiness).filter(happiness.year('2015')).all()
 
-    #Return the JSON representation of your dictionary.
-    return jasonify(results2105)
+    result2015 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2015""")
+
+    results = []
+
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2015:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
+        
+    return jsonify(results)
 
 
+    
 @app.route("/api/v1.0/2016")
 def happiness2016():
 
-    results2016 = session.query(happiness).filter(happiness.year('2016')).all()
+    result2016 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2016""")
 
-    return jasonify(results2016)
+    results = []
+
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2016:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
+
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/2017")
 def happiness2017():
 
-    results2017 = session.query(happiness).filter(happiness.year('2017')).all()
+    result2017 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2017""")
 
-    return jasonify(results2017)
+    results = []
+
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2017:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
+
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/2018")
 def happiness2018():
 
-    results2018 = session.query(happiness).filter(happiness.year('2018')).all()
+    result2018 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2018""")
 
-    return jasonify(results2018)
+    results = []
+
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2018:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
+
+    return jsonify(results)
 
 @app.route("/api/v1.0/2019")
 def happiness2019():
 
-    results2019 = session.query(happiness).filter(happiness.year('2019')).all()
+    result2019 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2019""")
 
-    return jasonify(results2019)
+    results = []
+
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2019:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
+
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/2020")
 def happiness2020():
     
-    results2020 = session.query(happiness).filter(happiness.year('2020')).all()
+    result2020 = con.execute("""with result as (
+        select h.country, wb.region, cc.countrycode, wb.capitalcity, wb.capitallat, wb.capitallong, h.happiness_rank, h.year, h.happiness_score, h.economy, h.family, h.health, h.freedom, h.trust, h.generosity, cc.population, cc.area, cc.gdp, wb.incomelevel from countrycodes cc, happiness h, wbcountries wb where cc.country = h.country and cc.country= wb.country )
+        select * from result where year=2020""")
 
-    return jasonify(results2020)
+    results = []
 
-@app.route("/api/v1.0/All")
-def happinessAll():
+    for country, region, countrycode, capitalcity, capitallat, capitallong, happiness_rank, year, happiness_score, economy, family, health, freedom, trust, generosity, population, area, gdp, incomelevel in result2020:
+                newjson = {}
+                newjson["country"] = country
+                newjson["region"] = region
+                newjson["country code"] = countrycode
+                newjson["capital city"] = capitalcity
+                newjson["country"] = country
+                newjson["latitude"] = capitallat
+                newjson["longitude"] = capitallong
+                newjson["happiness Rank"] = happiness_rank
+                newjson["Year"] = year
+                newjson["happiness_score"] = happiness_score
+                newjson["economy"] = economy
+                newjson["family"] = family
+                newjson["health"] = health
+                newjson["freedom"] = freedom
+                newjson["trust"] = trust
+                newjson["generosity"] = generosity
+                newjson["population"] = population
+                newjson["area"] = area
+                newjson["gdp"] = gdp
+                newjson["income level"] = incomelevel
+                results.append(newjson)
 
-     resultsall = session.query(happiness).all()
-    
-    return jasonify(resultsall)
+    return jsonify(results)
 
 
 if __name__ == '__main__':
